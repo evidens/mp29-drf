6df994ef2baa71cfab1e7871ca4cbb423476a539
F. Gabriel Gosselin <gabriel@evidens.ca>
2012-04-30 14:13:39 -0400

    Moving slides to a sub-directory

--- build.sh
+++ build.sh
@@ -6,10 +6,4 @@ pandoc --standalone \
     --output=presentation.html \
     --template=template.html \
     --css=style.css \
-    00_about.md \
-    01_rest.md \
-    02_whyDRF.md \
-    03_basic_start.md \
-    04_specify_resources.md \
-    05_nested_resources.md \
-    06_mixins.md
+    slides/*
